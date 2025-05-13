f0675be063fdb64025b3ae309e7eec40e8eb43d4
Yeongchan Won <138646017+Ordinary702@users.noreply.github.com>
2025-05-13 20:45:35 +0900

    Google Admob func

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,4 +102,7 @@ dependencies {
 
     implementation ("com.google.accompanist:accompanist-pager:0.32.0")
     implementation ("com.google.accompanist:accompanist-pager-indicators:0.32.0")
+
+    //구글 애드몹 API
+    implementation ("com.google.android.gms:play-services-ads:22.6.0")
 }
