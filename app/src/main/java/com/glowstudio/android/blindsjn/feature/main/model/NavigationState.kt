908f9331d11667021012d948336941477db3f8c2
gyshim <gyshim1004@gmail.com>
2025-05-19 05:17:08 +0900

    프리텐다드 폰트 적용

--- app/src/main/java/com/glowstudio/android/blindsjn/feature/main/model/NavigationState.kt
+++ app/src/main/java/com/glowstudio/android/blindsjn/feature/main/model/NavigationState.kt
@@ -18,7 +18,7 @@ data class NavigationState(
 sealed class Screen(val route: String, val title: String, val icon: ImageVector) {
     object Home : Screen("home", "홈화면", Icons.Default.Home)
     object Posts : Screen("board", "게시판 목록", Icons.Default.GridView)
-    object Popular : Screen("paymanagement", "매출 관리", Icons.Default.CreditCard)
+    object Popular : Screen("paymanagement", "매출 관리", Icons.Default.MonetizationOn)
     object Messages : Screen("message", "캘린더", Icons.Default.CalendarToday)
     object Profile : Screen("profile", "프로필", Icons.Default.Person)
 } 
